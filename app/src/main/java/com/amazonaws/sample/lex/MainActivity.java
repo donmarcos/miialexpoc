f84d20f012ca04054e9389db16be0d44bfc80116
unknown <marcos.alois.oliva@gmail.com>
2020-08-03 08:39:41 -0700

    AndoridX migrations refactor

--- app/src/main/java/com/amazonaws/sample/lex/MainActivity.java
+++ app/src/main/java/com/amazonaws/sample/lex/MainActivity.java
@@ -21,13 +21,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+import androidx.core.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.amazonaws.sample.lex.R;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
