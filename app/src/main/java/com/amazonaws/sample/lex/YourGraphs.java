b85ad45e5c5f192b3f701a905e332308a71c2757
/-\khile$h <iyerakhileshk@gmail.com>
2020-08-24 23:12:35 -0400

    backup_upload
    unsure file

---
+++ app/src/main/java/com/amazonaws/sample/lex/YourGraphs.java
@@ -0,0 +1,15 @@
+package com.amazonaws.sample.lex;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class YourGraphs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_graphs);
+
+    }
+}
