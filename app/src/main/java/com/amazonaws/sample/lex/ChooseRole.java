028956d2ce15e409f50ea6d93a2fa800faf0aafa
AgnesLL <li62232954@gmail.com>
2020-08-30 18:53:50 +0800

    welcome page

---
+++ app/src/main/java/com/amazonaws/sample/lex/ChooseRole.java
@@ -0,0 +1,13 @@
+package com.amazonaws.sample.lex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseRole extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_role);
+    }
+}
